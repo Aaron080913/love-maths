5239f55dbd823e0579538e14e91f39dfd580690f
Aaron080913 <>
2023-10-07 20:40:08 +0000

    added division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,10 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
     }else if(gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1 * num2, num2);
+    }
+     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -85,6 +88,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return[operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"]
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -123,4 +128,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "X";
 
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
